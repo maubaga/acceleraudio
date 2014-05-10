6763e628606d97ac2e28fe2112fe6829f23f1f26
michele.lissandrin@gmail.com <michele.lissandrin@gmail.com@58abe29f-9236-5f75-17eb-62633f9ce754>
2014-05-10 11:11:41 +0000

    I completed the management of data persistence in the class PrefActivity

--- branches/AccelerAudio/src/main/acceleraudio/PrefActivity.java
+++ branches/AccelerAudio/src/main/acceleraudio/PrefActivity.java
@@ -8,12 +8,14 @@ package main.acceleraudio;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.*;
 import android.os.Build;
 
 public class PrefActivity extends ActionBarActivity {
@@ -27,6 +29,29 @@ public class PrefActivity extends ActionBarActivity {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
+		
+		//recupero gli stati dei componenti utilizzando SharedPreferences
+		SharedPreferences preferences = getPreferences(MODE_PRIVATE); 
+		boolean pref_cbX = preferences.getBoolean("cBoxSelectX", false);
+		boolean pref_cbY = preferences.getBoolean("cBoxSelectY", false);
+		boolean pref_cbZ = preferences.getBoolean("cBoxSelectZ", false);
+		String pref_rate = preferences.getString("eTextSampleRate", null);
+		String pref_maxRec = preferences.getString("eTextMaxRec", null);
+		String pref_upsampl = preferences.getString("eTextUpsampling", null);
+		
+		//assegno i valori degli stati ai relativi componenti
+		CheckBox cbX = (CheckBox)findViewById(R.id.checkBoxX); 
+		cbX.setChecked(pref_cbX);
+		CheckBox cbY = (CheckBox)findViewById(R.id.checkBoxY); 
+		cbY.setChecked(pref_cbY);
+		CheckBox cbZ = (CheckBox)findViewById(R.id.checkBoxZ); 
+		cbZ.setChecked(pref_cbZ);
+		EditText et_rate = (EditText)findViewById(R.id.sampleRate);
+		et_rate.setText(pref_rate);
+		EditText et_maxRec = (EditText)findViewById(R.id.max_rec);
+		et_maxRec.setText(pref_maxRec);
+		EditText et_upsampl = (EditText)findViewById(R.id.v_upsamping);
+		et_upsampl.setText(pref_upsampl);
 	}
 
 	@Override
@@ -64,6 +89,44 @@ public class PrefActivity extends ActionBarActivity {
 					false);
 			return rootView;
 		}
+	}
+	
+	protected void onPause(){
+		super.onPause();
+		
+		//utilizzo SharedPreferece e il relativo editor per salvare lo stato dei componenti
+		SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+		SharedPreferences.Editor editor = preferences.edit();
+		
+		//stato dei checkbox relativi agli assi selezionati
+		CheckBox cbX = (CheckBox)findViewById(R.id.checkBoxX); 
+		boolean pref_cbX = cbX.isChecked();
+		CheckBox cbY = (CheckBox)findViewById(R.id.checkBoxY); 
+		boolean pref_cbY = cbY.isChecked();
+		CheckBox cbZ = (CheckBox)findViewById(R.id.checkBoxZ); 
+		boolean pref_cbZ = cbZ.isChecked();
+		
+		//stato del box di testo numerico relativo alla freq. di campionamento
+		EditText et_rate = (EditText)findViewById(R.id.sampleRate);
+		String pref_rate = et_rate.getText().toString();
+		
+		//stato del box di testo numerico relativo alla massima durata della registrazione
+		EditText et_maxRec = (EditText)findViewById(R.id.max_rec);
+		String pref_maxRec = et_maxRec.getText().toString();
+		
+		//stato del box di testo numerico relativo al parametro di interpolazione
+		EditText et_upsampl = (EditText)findViewById(R.id.v_upsamping);
+		String pref_upsampl = et_upsampl.getText().toString();
+		
+		//salvo lo stato
+		editor.putBoolean("cBoxSelectX", pref_cbX);
+		editor.putBoolean("cBoxSelectY", pref_cbY);
+		editor.putBoolean("cBoxSelectZ", pref_cbZ);
+		editor.putString("eTextSampleRate", pref_rate);
+		editor.putString("eTextMaxRec", pref_maxRec);
+		editor.putString("eTextUpsampling", pref_upsampl);
+		
+		editor.commit();
 	}
 
 }
