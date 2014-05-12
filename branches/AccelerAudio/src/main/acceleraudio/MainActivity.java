46a8caf2a38346f8cb7efbbeacc8fb89e68c6bd7
michelelissandrin <michelelissandrin@58abe29f-9236-5f75-17eb-62633f9ce754>
2014-05-12 12:54:45 +0000

    Added to mainActivity intent to connect prefActivity and created the folder layout-land to handle the landscape mode

--- branches/AccelerAudio/src/main/acceleraudio/MainActivity.java
+++ branches/AccelerAudio/src/main/acceleraudio/MainActivity.java
@@ -40,7 +40,9 @@ public class MainActivity extends ActionBarActivity {
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
-			return true;
+			Intent intent=new Intent(this, PrefActivity.class);
+			startActivity(intent);
+			
 		}
 		
 		if (id == R.id.action_new) {
