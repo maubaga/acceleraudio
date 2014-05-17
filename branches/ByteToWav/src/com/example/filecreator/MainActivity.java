9138519e8f14a0d2641bf81451669cf2cea3ae56
bluemango92@gmail.com <bluemango92@gmail.com@58abe29f-9236-5f75-17eb-62633f9ce754>
2014-05-17 11:51:54 +0000

    Improve defining all variables and also add some comment.

--- branches/ByteToWav/src/com/example/filecreator/MainActivity.java
+++ branches/ByteToWav/src/com/example/filecreator/MainActivity.java
@@ -16,10 +16,17 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	//name of the .wav file
 	private String file = "song.wav";
+
+	//all file parameters
+	private byte bits_per_sample = 8; // 8, 16...
+	private byte num_channels = 1; // 1 = mono, 2 = stereo
+	private long sample_rate = 8000; // 8000, 44100...
+
 	FileOutputStream fout;
 	Random r = new Random();
-	
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -28,18 +35,18 @@ public class MainActivity extends Activity {
 
 		if (savedInstanceState == null) {
 			getFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
+			.add(R.id.container, new PlaceholderFragment()).commit();
 		}
-		
+
 		try {
-	        fout = openFileOutput(file,MODE_WORLD_READABLE);
-	     } catch (Exception e) {
-	        // TODO Auto-generated catch block
-	        e.printStackTrace();
-	     }
-		
-		
-		
+			fout = openFileOutput(file,MODE_WORLD_READABLE);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+
 	}
 
 	@Override
@@ -61,99 +68,110 @@ public class MainActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
-	
+
+
 	//https://ccrma.stanford.edu/courses/422/projects/WaveFormat/
+	//write the .wav file
 	public void create(View view){
-		
-	      try {
-	         FileOutputStream fOut = openFileOutput(file,MODE_WORLD_READABLE);
-	         WriteWaveFileHeader(fOut, 100000, 100036, 8000, 1, 16000);
-	         byte[] dataAdded = new byte[50000];
-	         for (int i = 0; i < dataAdded.length; i++)
-	        	 dataAdded[i] = (byte)r.nextInt(127);
-	         fOut.write(dataAdded);
-	         fOut.close();
-	         Toast.makeText(getBaseContext(),"Il file è stato creato!",
-	         Toast.LENGTH_SHORT).show();
-	      } catch (Exception e) {
-	         // TODO Auto-generated catch block
-	         e.printStackTrace();
-	      }
-		       
+
+		try {
+
+			//This is the array with our data
+			byte[] dataAdded = new byte[50000];
+
+			FileOutputStream fOut = openFileOutput(file,MODE_WORLD_READABLE);
+			WriteWaveFileHeader(fOut, (dataAdded.length*num_channels*(bits_per_sample/8)), 
+					(36+(dataAdded.length*num_channels*(bits_per_sample/8))), 
+					sample_rate, num_channels, 
+					(sample_rate*num_channels*(bits_per_sample/8)));
+			for (int i = 0; i < dataAdded.length; i++)
+				dataAdded[i] = (byte)r.nextInt(127);
+			fOut.write(dataAdded);
+			fOut.close();
+			Toast.makeText(getBaseContext(),"Il file è stato creato!",
+					Toast.LENGTH_SHORT).show();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
-	
-//	public void read(View view){
-//	      try{
-//	         FileInputStream fin = openFileInput(file);
-//	         int c;
-//	         String temp="";
-//	         while( (c = fin.read()) != -1){
-//	            temp = temp + Character.toString((char)c);
-//	         }
-//	         testo.setText(temp);
-//	         Toast.makeText(getBaseContext(),"file read",
-//	         Toast.LENGTH_SHORT).show();
-//
-//	      }catch(Exception e){
-//
-//	      }
-//	   }
-	
+
+	//read the .wav file
+	// TODO use MediaPlayer to reproduce the song
+
+	//	public void read(View view){
+	//	      try{
+	//	         FileInputStream fin = openFileInput(file);
+	//	         int c;
+	//	         String temp="";
+	//	         while( (c = fin.read()) != -1){
+	//	            temp = temp + Character.toString((char)c);
+	//	         }
+	//	         testo.setText(temp);
+	//	         Toast.makeText(getBaseContext(),"file read",
+	//	         Toast.LENGTH_SHORT).show();
+	//
+	//	      }catch(Exception e){
+	//
+	//	      }
+	//	   }
+
+	//define the header of the .wav file. DON'T CHANGE IT!
 	private void WriteWaveFileHeader(
-            FileOutputStream out, long totalAudioLen,
-            long totalDataLen, long longSampleRate, int channels,
-            long byteRate) throws IOException {
+			FileOutputStream out, long totalAudioLen,
+			long totalDataLen, long longSampleRate, int channels,
+			long byteRate) throws IOException {
 
-    byte[] header = new byte[44];
+		byte[] header = new byte[44];
 
-    header[0] = 'R';  // RIFF/WAVE header
-    header[1] = 'I';
-    header[2] = 'F';
-    header[3] = 'F';
-    header[4] = (byte) (totalDataLen & 0xff);
-    header[5] = (byte) ((totalDataLen >> 8) & 0xff);
-    header[6] = (byte) ((totalDataLen >> 16) & 0xff);
-    header[7] = (byte) ((totalDataLen >> 24) & 0xff);
-    header[8] = 'W';
-    header[9] = 'A';
-    header[10] = 'V';
-    header[11] = 'E';
-    header[12] = 'f';  // 'fmt ' chunk
-    header[13] = 'm';
-    header[14] = 't';
-    header[15] = ' ';
-    header[16] = 16;  // 4 bytes: size of 'fmt ' chunk
-    header[17] = 0;
-    header[18] = 0;
-    header[19] = 0;
-    header[20] = 1;  // format = 1
-    header[21] = 0;
-    header[22] = (byte) channels;
-    header[23] = 0;
-    header[24] = (byte) (longSampleRate & 0xff);
-    header[25] = (byte) ((longSampleRate >> 8) & 0xff);
-    header[26] = (byte) ((longSampleRate >> 16) & 0xff);
-    header[27] = (byte) ((longSampleRate >> 24) & 0xff);
-    header[28] = (byte) (byteRate & 0xff);
-    header[29] = (byte) ((byteRate >> 8) & 0xff);
-    header[30] = (byte) ((byteRate >> 16) & 0xff);
-    header[31] = (byte) ((byteRate >> 24) & 0xff);
-    header[32] = (byte) (2 * 16 / 8);  // block align
-    header[33] = 0;
-    header[34] = 16;  // bits per sample
-    header[35] = 0;
-    header[36] = 'd';
-    header[37] = 'a';
-    header[38] = 't';
-    header[39] = 'a';
-    header[40] = (byte) (totalAudioLen & 0xff);
-    header[41] = (byte) ((totalAudioLen >> 8) & 0xff);
-    header[42] = (byte) ((totalAudioLen >> 16) & 0xff);
-    header[43] = (byte) ((totalAudioLen >> 24) & 0xff);
+		header[0] = 'R';  // RIFF/WAVE header
+		header[1] = 'I';
+		header[2] = 'F';
+		header[3] = 'F';
+		header[4] = (byte) (totalDataLen & 0xff);
+		header[5] = (byte) ((totalDataLen >> 8) & 0xff);
+		header[6] = (byte) ((totalDataLen >> 16) & 0xff);
+		header[7] = (byte) ((totalDataLen >> 24) & 0xff);
+		header[8] = 'W';
+		header[9] = 'A';
+		header[10] = 'V';
+		header[11] = 'E';
+		header[12] = 'f';  // 'fmt ' chunk
+		header[13] = 'm';
+		header[14] = 't';
+		header[15] = ' ';
+		header[16] = 16;  // 4 bytes: size of 'fmt ' chunk
+		header[17] = 0;
+		header[18] = 0;
+		header[19] = 0;
+		header[20] = 1;  // format = 1
+		header[21] = 0;
+		header[22] = (byte) channels;
+		header[23] = 0;
+		header[24] = (byte) (longSampleRate & 0xff);
+		header[25] = (byte) ((longSampleRate >> 8) & 0xff);
+		header[26] = (byte) ((longSampleRate >> 16) & 0xff);
+		header[27] = (byte) ((longSampleRate >> 24) & 0xff);
+		header[28] = (byte) (byteRate & 0xff);
+		header[29] = (byte) ((byteRate >> 8) & 0xff);
+		header[30] = (byte) ((byteRate >> 16) & 0xff);
+		header[31] = (byte) ((byteRate >> 24) & 0xff);
+		header[32] = (byte) (2 * 16 / 8);  // block align
+		header[33] = 0;
+		header[34] = bits_per_sample;  // bits per sample
+		header[35] = 0;
+		header[36] = 'd';
+		header[37] = 'a';
+		header[38] = 't';
+		header[39] = 'a';
+		header[40] = (byte) (totalAudioLen & 0xff);
+		header[41] = (byte) ((totalAudioLen >> 8) & 0xff);
+		header[42] = (byte) ((totalAudioLen >> 16) & 0xff);
+		header[43] = (byte) ((totalAudioLen >> 24) & 0xff);
 
-    out.write(header, 0, 44);
-}
+		out.write(header, 0, 44);
+	}
 
 	/**
 	 * A placeholder fragment containing a simple view.
@@ -168,7 +186,7 @@ public class MainActivity extends Activity {
 				Bundle savedInstanceState) {
 			View rootView = inflater.inflate(R.layout.fragment_main, container,
 					false);
-			
+
 			return rootView;
 		}
 	}
