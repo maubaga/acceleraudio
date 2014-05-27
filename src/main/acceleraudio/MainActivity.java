abf2140d12ad810d9e385c434a625a644a1db2c0
maubaga92 <maubaga92@gmail.com>
2014-05-27 20:13:17 +0000

    MainActivity: Access to the database and show the file stored in the app. DBOpenHelper: Now data and time are different fields.

--- src/main/acceleraudio/MainActivity.java
+++ src/main/acceleraudio/MainActivity.java
@@ -1,25 +1,48 @@
 package main.acceleraudio;
 
+import static android.provider.BaseColumns._ID;
+import static main.acceleraudio.DBOpenHelper.LAST_MODIFY_DATE;
+import static main.acceleraudio.DBOpenHelper.NAME;
+import static main.acceleraudio.DBOpenHelper.TABLE;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
-	
+
+	private boolean resumeHasRun = false;
+	private DBOpenHelper dbHelper;
+	static String folder;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
+		setContentView(R.layout.fragment_main);
+		//		if (savedInstanceState == null) {
+		//			getSupportFragmentManager().beginTransaction()
+		//					.add(R.id.container, new PlaceholderFragment()).commit();
+		//		}
+		folder = getApplicationContext().getFilesDir().getPath() + "/";
+
+		dbHelper = new DBOpenHelper(this);
+		Cursor cursor = getSessions();
+		showSessions(cursor);
+
 	}
 
 	@Override 
@@ -40,7 +63,7 @@ public class MainActivity extends ActionBarActivity {
 			Intent intent = new Intent(this, PrefActivity.class);
 			startActivity(intent);			
 		}
-		
+
 		if (id == R.id.action_new) {
 			Intent intent = new Intent(this, RecordActivity.class);
 			startActivity(intent); 
@@ -48,21 +71,133 @@ public class MainActivity extends ActionBarActivity {
 		return super.onOptionsItemSelected(item);
 	}
 
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
+	final static String IMG_NAME = folder + NAME + ".png";
+	private static String[] FROM = { _ID, NAME, NAME, LAST_MODIFY_DATE };
+	private static String ORDER_BY = NAME + " ASC";
 
-		public PlaceholderFragment() {
-		}
+	private Cursor getSessions() {
+		// Get all of the notes from the database and create the item list
+		SQLiteDatabase db = dbHelper.getReadableDatabase();
+		Cursor cursor = db.query(TABLE, FROM, null, null, null, null, ORDER_BY);
+		startManagingCursor(cursor);
 
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			return rootView;
-		}
+		return cursor;
 	}
+
+	private void showSessions(Cursor cursor) {
+
+		LinearLayout main_container = (LinearLayout)findViewById(R.id.main_container);
+		main_container.removeAllViews();
+
+		for(int i = 0; i < cursor.getCount(); i++){
+
+			cursor.moveToPosition(i);
+
+			LinearLayout session = new LinearLayout(this);
+			session.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 48, getResources().getDisplayMetrics())));
+			session.setOrientation(LinearLayout.HORIZONTAL);
+
+			ImageView img = new ImageView(this);
+			LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+					(int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 40, getResources().getDisplayMetrics()), 
+					(int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 40, getResources().getDisplayMetrics()));
+			params.gravity = Gravity.CENTER_VERTICAL;
+			img.setImageURI(Uri.parse(folder + cursor.getString(cursor.getColumnIndex(NAME)) + ".png"));
+			img.setLayoutParams(params);
+
+			TextView name = new TextView(this);
+			LinearLayout.LayoutParams params2 = new LinearLayout.LayoutParams(
+					(int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 120, getResources().getDisplayMetrics()), 
+					LayoutParams.WRAP_CONTENT);
+			params2.gravity = Gravity.CENTER_VERTICAL;
+			name.setPadding((int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 5, getResources().getDisplayMetrics()), 0, 0, 0);
+			name.setTextSize(16);
+			name.setText(cursor.getString(cursor.getColumnIndex(NAME)));
+			name.setLayoutParams(params2);
+			final String session_name = name.getText().toString();
+
+			TextView date = new TextView(this);
+			LinearLayout.LayoutParams params3 = new LinearLayout.LayoutParams(
+					(int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 120, getResources().getDisplayMetrics()), 
+					LayoutParams.WRAP_CONTENT);
+			params3.gravity = Gravity.CENTER_VERTICAL;
+			date.setPadding((int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 5, getResources().getDisplayMetrics()), 0, 0, 0);
+			date.setTextSize(16);
+			date.setText(cursor.getString(cursor.getColumnIndex(LAST_MODIFY_DATE)));
+			date.setLayoutParams(params3);
+
+			ImageButton play = new ImageButton(this);
+			play.setLayoutParams(new LayoutParams((int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 48, getResources().getDisplayMetrics()), (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 48, getResources().getDisplayMetrics())));
+			play.setImageResource(R.drawable.media_play);
+			play.setScaleType(ScaleType.FIT_CENTER);
+			play.setOnClickListener(new OnClickListener() {
+				 
+				@Override
+				public void onClick(View arg0) {
+	 
+				   startSession(arg0, session_name);
+	 
+				}
+	 
+			});
+
+			View line = new View(this);
+			line.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1, getResources().getDisplayMetrics())));
+			line.setBackgroundColor(0x227a7a7a);
+
+			session.addView(img);
+			session.addView(name);
+			session.addView(date);
+			session.addView(play);
+			main_container.addView(session);
+			main_container.addView(line);
+
+		}
+
+	}
+	
+	private void startSession(View view, String name){
+		
+		Intent intent = new Intent(this, PlayActivity.class);
+		intent.putExtra("session_name", name);
+		startActivity(intent);
+		
+	}
+
+	@Override
+	protected void onResume(){
+
+		super.onResume();
+		if (!resumeHasRun) {
+	        resumeHasRun = true;
+	        return;
+	    } else{
+	    	
+	    	folder = getApplicationContext().getFilesDir().getPath() + "/";
+
+			dbHelper = new DBOpenHelper(this);
+			Cursor cursor = getSessions();
+			showSessions(cursor);
+	    	
+	    }
+
+	}
+
+	//	/**
+	//	 * A placeholder fragment containing a simple view.
+	//	 */
+	//	public static class PlaceholderFragment extends Fragment {
+	//
+	//		public PlaceholderFragment() {
+	//		}
+	//
+	//		@Override
+	//		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	//				Bundle savedInstanceState) {
+	//			View rootView = inflater.inflate(R.layout.fragment_main, container,
+	//					false);
+	//			return rootView;
+	//		}
+	//	}
 
 }
