5169567eb1951ecf4a8d560fa0c39656e23606d3
maubaga92 <maubaga92@gmail.com>
2014-06-02 16:50:59 +0000

    PlayActivity/PlayService: Fixed play in background!

--- src/main/acceleraudio/PlayerService.java
+++ src/main/acceleraudio/PlayerService.java
@@ -1,9 +1,12 @@
 package main.acceleraudio;
 
+import java.io.IOException;
+
 import android.app.PendingIntent; 
 import android.app.Service; 
 import android.app.Notification; 
 import android.support.v4.app.NotificationCompat; 
+import android.widget.Toast;
 import android.content.Intent; 
 import android.media.MediaPlayer; 
 import android.net.Uri;
@@ -12,7 +15,8 @@ import android.os.IBinder;
 public class PlayerService extends Service{
 	public static String PLAY_START = "play_start"; 
 	public static String PLAY_STOP = "play_stop";
-	public static String PATH;
+	public static String PATH = "path_directory";
+	private String sessionPlay;
 	private MediaPlayer myPlayer = null; 
 	private boolean isPlaying = false; 
 
@@ -25,19 +29,33 @@ public class PlayerService extends Service{
 	@Override 
 	public int onStartCommand(Intent intent, int flags, int startId) 
 	{ 
-		if(intent.getBooleanExtra(PLAY_START, false)) play(); 
+		if(intent.getBooleanExtra(PLAY_START, false)) {
+			sessionPlay = intent.getStringExtra(PATH);
+			play(); 
+		}
+		else{
+			stop();
+		}
 		return Service.START_STICKY; 
 	}
 
 	private void play() { 
 		if(isPlaying)
 			return; 
+		
+		try {
+			isPlaying = true;
+			myPlayer = new MediaPlayer(); 
+			myPlayer.setDataSource(sessionPlay);
+			myPlayer.prepare();
+			myPlayer.setLooping(true); 
+			myPlayer.start(); 
 
-		isPlaying = true;
-		myPlayer = MediaPlayer.create(this, Uri.parse(PATH)); 
-		myPlayer.setLooping(true); 
-		myPlayer.start(); 
-
+		} catch (IOException e) {
+			Toast.makeText(getBaseContext(),"prepare failed",
+					Toast.LENGTH_SHORT).show();
+		}
+		
 		// Runs this service in the foreground, 
 		// supplying the ongoing notification to be shown to the user 
 		Intent intent = new Intent(this, PlayActivity.class); 
