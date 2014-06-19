d3367b0c9c8e6c7541c1f3c0573e5ec2f08fbb49
maubaga92@gmail.com <maubaga92@gmail.com@58abe29f-9236-5f75-17eb-62633f9ce754>
2014-06-19 21:53:13 +0000

    PlayerService: Fixed notification.

--- branches/AccelerAudio/src/main/acceleraudio/PlayActivity.java
+++ branches/AccelerAudio/src/main/acceleraudio/PlayActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
 import android.os.SystemClock;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
